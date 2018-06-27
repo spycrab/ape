5d651f73998df14dc1a94792986bc4d52c6b36c8
spycrab <spycrab@users.noreply.github.com>
2018-06-27 15:40:40 +0200

    Common/Logger: Fix windows paths

--- Source/Common/Logger.cpp
+++ Source/Common/Logger.cpp
@@ -4,14 +4,22 @@
 
 #include "Common/Logger.h"
 
+#include "Common/File.h"
+
 #include <iostream>
 #include <string>
 
 void __MSG(std::string type, std::string file, int line, std::string msg)
 {
   auto StripPath = [](std::string file) {
+    file = Util::Path::ToUnix(file);
     static const std::string src_prefix = "Source/";
+    static const std::string src_prefix_lc = "source/";
+
     auto index = file.find(src_prefix);
+
+    if (index == std::string::npos)
+      index = file.find(src_prefix_lc);
 
     if (index == std::string::npos)
       return file;
